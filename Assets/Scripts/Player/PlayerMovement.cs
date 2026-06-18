14ed381ed2c6c3e2e7fe2f6f51f6d44e45299d12
Francisco <132166496+4FranJonas2@users.noreply.github.com>
2026-06-18 14:10:32 -0300

    Update PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,21 +3,55 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 5f;
 
     private Rigidbody2D rb;
     private Vector2 movement;
 
+    private bool isGrounded = true;
+
+    private Animator animator;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        float move = Input.GetAxisRaw("Horizontal");
 
-        movement.Normalize();
+        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+
+        if (Input.GetKey(KeyCode.W) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(move));
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 
     private void FixedUpdate()
