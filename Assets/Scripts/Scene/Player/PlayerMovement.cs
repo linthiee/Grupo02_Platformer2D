7236cb98b0d490771ba8ae4961a68a16474ad48f
Francisco <132166496+4FranJonas2@users.noreply.github.com>
2026-06-20 17:29:11 -0300

    Add deleted playerr script

---
+++ Assets/Scripts/Scene/Player/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+
+    private Rigidbody2D rb;
+
+    private bool isGrounded = true;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+
+        animator.SetFloat("Speed", Mathf.Abs(move));
+
+        animator.SetBool("IsGround", isGrounded);
+
+        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        animator.SetFloat("VerticalSpeed", rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Choqué con: " + collision.gameObject.name);
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            Debug.Log("Player is grounded.");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
