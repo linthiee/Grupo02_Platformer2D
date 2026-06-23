f50e2251aa3914f12376025ebc76a108f5247234
Francisco <132166496+4FranJonas2@users.noreply.github.com>
2026-06-23 16:15:42 -0300

    Added Coin logic

---
+++ Assets/Scripts/Coin/CoinLogic.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class CoinLogic : MonoBehaviour
+{
+    [SerializeField] private int coinValue = 1;
+
+    private IEventBus _eventBus;
+
+    private void Awake()
+    {
+        _eventBus = ServiceLoader.GetService<EventBus>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+           _eventBus.Publish(new EventBus.CoinCollectedEvent(coinValue));
+
+            Destroy(gameObject);
+        }
+    }
+}
