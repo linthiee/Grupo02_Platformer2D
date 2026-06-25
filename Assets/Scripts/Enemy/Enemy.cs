f400acd5227522a5a954ac7d2afde73a19a7308e
elu <eluujazmin@gmail.com>
2026-06-25 10:29:33 -0300

    add patrolling via waypoints

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour
@@ -31,19 +30,59 @@ public abstract class Enemy : MonoBehaviour
         transform.position = new Vector3(transform.position.x + (speed * Time.deltaTime * direction),
             transform.position.y, transform.position.z);
 
+        Debug.Log("patroling");
+    }
+
+    protected virtual void Patrol(Transform pointA, Transform pointB, ref Transform currentTarget)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, currentTarget.position, speed * Time.deltaTime);
+
+        if (currentTarget.position.x > transform.position.x && direction == -1)
+        {
+            direction = 1;
+            Flip();
+        }
+        else if (currentTarget.position.x < transform.position.x && direction == 1)
+        {
+            direction = -1;
+            Flip();
+        }
+
+        if (Mathf.Abs(currentTarget.position.x - transform.position.x) <= 0.5f)
+        {
+            if (currentTarget == pointA)
+            {
+                currentTarget = pointB;
+            }
+            else
+            {
+                currentTarget = pointA;
+            }
+            Debug.Log("changing target");
+        }
         animator.SetTrigger("walk");
+
+        Debug.Log($"current target: {currentTarget.position.x}, position: {transform.position.x}, distance: {Mathf.Abs(currentTarget.position.x - transform.position.x)}");
     }
 
     protected virtual void OnCollisionStay2D(Collision2D collision)
     {
+        Debug.Log("collision");
+
         Vector2 collisionNormal = collision.GetContact(0).normal;
 
         if (Mathf.Abs(collisionNormal.x) > 0.5f && collision.gameObject.CompareTag("Ground"))
         {
-                Debug.Log("vvvvvvvvvvvvvvvvvvvvvvv");
-                direction = -direction;
+            Debug.Log("vvvvvvvvvvvvvvvvvvvvvvv");
+            direction = -direction;
+            Flip();
         }
     }
 
     protected abstract void Run();
+
+    private void Flip()
+    {
+        transform.Rotate(0f, 180f, 0f);
+    }
 }
