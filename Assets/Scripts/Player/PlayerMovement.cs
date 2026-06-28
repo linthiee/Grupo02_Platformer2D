26cb6a2d2bd1f9e94426e934e428b2ad80df42fa
Francisco <132166496+4FranJonas2@users.noreply.github.com>
2026-06-28 16:41:49 -0300

    Update PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -17,7 +17,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        PlayerSpawnPoint playerSpawnpoint = FindObjectOfType<PlayerSpawnPoint>();
+        PlayerSpawnPoint playerSpawnpoint = FindFirstObjectByType<PlayerSpawnPoint>();
       
         if (playerSpawnpoint != null)
         {
